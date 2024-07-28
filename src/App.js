eb1dd8fd4a2d4fb6d375bca8550f57ebbd958cdb
Ian Ker Arado Cajes <icajes@yotpo.com>
2024-07-29 00:58:11 +0900

    2nd

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = {productid};
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
