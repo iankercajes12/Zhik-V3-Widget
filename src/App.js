1aef64a3c3e8fda56e5fc1e711bbe7f8b09b97c9
Ian Ker Arado Cajes <icajes@yotpo.com>
2024-08-18 19:33:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "453363";
+  let InstanceId_Widget = "823925";
   let InstanceId_QA = "";
-  let InstanceId_SEO = "";
+  let InstanceId_SEO = "833117";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "";
+  let InstanceId_StarRating = "833116";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "7346754715857";
+  let ProductId = "7036434940043";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
